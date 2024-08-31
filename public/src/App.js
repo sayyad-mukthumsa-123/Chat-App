559e29ac7fc926091270ec214543c1fd46daed92
sayyad-mukthumsa-123 <sayyad.mukthumsa@gmail.com>
2024-08-31 13:56:24 +0530

    Project updated

--- public/src/App.js
+++ public/src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 export default function App() {
+  useEffect(() => {
+    document.title = 'BuzzChat';
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
